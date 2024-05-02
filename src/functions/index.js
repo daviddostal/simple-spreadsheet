454227d23ad3fcb79a41dafa9ab84a9070ea675d
David <david.dostal1112@gmail.com>
2024-05-02 16:21:09 +0200

    Refactor: make all spreadsheet functions named
    Using named functions instead of anonymous ones might make it easier to debug them.

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,5 +1,5 @@
 export const builtinFunctions = {
-    SUM: (...args) => {
+    SUM: function SUM(...args) {
         let sum = 0;
         for (let arg of args.flat()) {
             if (typeof (arg) === 'number')
@@ -10,7 +10,7 @@ export const builtinFunctions = {
         return sum;
     },
 
-    AVERAGE: (...args) => {
+    AVERAGE: function AVERAGE(...args) {
         let sum = 0;
         let count = 0;
         for (let arg of args.flat()) {
@@ -26,14 +26,14 @@ export const builtinFunctions = {
 
     IF: {
         isLazy: true,
-        function: function(condition, trueBranch, falseBranch) {
+        function: function IF(condition, trueBranch, falseBranch) {
             return condition() ? trueBranch() : falseBranch()
         }
     },
 
     OR: {
         isLazy: true,
-        function: function(...conditions) {
+        function: function OR(...conditions) {
             if (conditions.length === 0) throw new Error(`OR() expects at least 1 argument.`);
             return conditions.some(condition => condition());
         }
@@ -41,24 +41,24 @@ export const builtinFunctions = {
 
     AND: {
         isLazy: true,
-        function: function(...conditions) {
+        function: function AND(...conditions) {
             if (conditions.length === 0) throw new Error(`AND() expects at least 1 argument.`);
             return conditions.every(condition => condition());
         }
     },
 
-    NOT: function(boolean, ...rest) {
+    NOT: function NOT(boolean, ...rest) {
         if (rest.length !== 0) throw new Error(`NOT() expects exactly 1 argument.`);
         return !boolean;
     },
 
-    PI: function () {
+    PI: function PI() {
         if (arguments.length > 0)
             throw new Error(`PI() expects 0 arguments, got ${arguments.length} arguments.`);
         return 3.141592653589793;
     },
 
-    E: function () {
+    E: function E() {
         if (arguments.length > 0)
             throw new Error(`E() expects 0 arguments, got ${arguments.length} arguments.`);
         return 2.718281828459045;
